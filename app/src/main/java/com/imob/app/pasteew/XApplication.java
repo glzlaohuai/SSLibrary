b23e2ec160f49183c397ba25368341cbb1dc4408
glzlaohuai <494763134@qq.com>
2021-12-10 16:18:36 +0800

    refactor

--- app/src/main/java/com/imob/app/pasteew/XApplication.java
+++ app/src/main/java/com/imob/app/pasteew/XApplication.java
@@ -4,9 +4,9 @@ import android.app.Application;
 import android.content.Context;
 import android.os.Build;
 
-import com.badzzz.pasteany.core.nsd.peer.client.ConnectedClientsHandler;
 import com.badzzz.pasteany.core.nsd.peer.ConnectedPeerEventListenerAdapter;
 import com.badzzz.pasteany.core.nsd.peer.ConnectedPeerEventListenerWrapper;
+import com.badzzz.pasteany.core.nsd.peer.client.ConnectedClientsHandler;
 import com.badzzz.pasteany.core.utils.Constants;
 import com.badzzz.pasteany.core.wrap.PreferenceManagerWrapper;
 import com.badzzz.pasteany.lib.core.android.AndroidPlatformManager;
@@ -26,19 +26,22 @@ public class XApplication extends Application {
         super.onCreate();
         context = getApplicationContext();
 
-        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(this);
-        strategy.setEnableCatchAnrTrace(true);
-        CrashReport.initCrashReport(getApplicationContext(), Constants.Others.BUGLY_APP_ID, true, strategy);
-
-        AndroidPlatformManager androidPlatformManager = new AndroidPlatformManager(this);
+        initBugly();
         new ANRWatchDog().start();
 
+        AndroidPlatformManager androidPlatformManager = new AndroidPlatformManager(this);
         // TODO: 2021/11/30 just for tests, should be removed later
         PreferenceManagerWrapper.getInstance().saveDeviceName(Build.BRAND + "#" + Build.DEVICE.toString());
         PreferenceManagerWrapper.getInstance().saveServiceName("a_test_service_name");
-
         androidPlatformManager.initPlatform();
         ConnectedClientsHandler.monitorConnectedPeersEvents(new ConnectedPeerEventListenerWrapper(new ConnectedPeerEventListenerAdapter(), true));
+    }
+
+
+    private void initBugly() {
+        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(this);
+        strategy.setEnableCatchAnrTrace(true);
+        CrashReport.initCrashReport(getApplicationContext(), Constants.Others.BUGLY_APP_ID, true, strategy);
     }
 
 }

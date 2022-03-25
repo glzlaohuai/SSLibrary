e7c83f8ff230708dc10033245f268f1120b1c09c
glzlaohuai <494763134@qq.com>
2022-03-25 11:11:37 +0800

    set -> concurrentQueue

--- lib_ss/src/main/java/com/imob/lib/sslib/server/ServerListenerGroup.java
+++ lib_ss/src/main/java/com/imob/lib/sslib/server/ServerListenerGroup.java
@@ -2,60 +2,60 @@ package com.imob.lib.sslib.server;
 
 import com.imob.lib.sslib.peer.Peer;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ServerListenerGroup implements ServerListener {
 
-    private final Set<ServerListener> set = new LinkedHashSet<>();
+    private final Queue<ServerListener> queue = new ConcurrentLinkedQueue<>();
 
     public void add(ServerListener listener) {
-        if (listener != null) {
-            set.add(listener);
+        if (listener != null && !queue.contains(listener)) {
+            queue.add(listener);
         }
     }
 
     public void remove(ServerListener listener) {
         if (listener != null) {
-            set.remove(listener);
+            queue.remove(listener);
         }
     }
 
     public void clear() {
-        set.clear();
+        queue.clear();
     }
 
     @Override
     public void onCreated(ServerNode serverNode) {
-        for (ServerListener listener : set) {
+        for (ServerListener listener : queue) {
             listener.onCreated(serverNode);
         }
     }
 
     @Override
     public void onCreateFailed(Exception exception) {
-        for (ServerListener listener : set) {
+        for (ServerListener listener : queue) {
             listener.onCreateFailed(exception);
         }
     }
 
     @Override
     public void onDestroyed(ServerNode serverNode) {
-        for (ServerListener listener : set) {
+        for (ServerListener listener : queue) {
             listener.onDestroyed(serverNode);
         }
     }
 
     @Override
     public void onCorrupted(ServerNode serverNode, String msg, Exception e) {
-        for (ServerListener listener : set) {
+        for (ServerListener listener : queue) {
             listener.onCorrupted(serverNode, msg, e);
         }
     }
 
     @Override
     public void onIncomingClient(ServerNode serverNode, Peer peer) {
-        for (ServerListener listener : set) {
+        for (ServerListener listener : queue) {
             listener.onIncomingClient(serverNode, peer);
         }
     }
